037d1d55b928853f90ede5ded8dac8a80ee4f395
Miliutkin_AV <Miliutkin_AV@st.by>
2022-06-26 01:26:24 +0300

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import { useGetSalesQuery } from "./generated/schema";
 
-function App() {
+export const App = () => {
+  const { data } = useGetSalesQuery();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data?.sales?.map(({ price, id }) => (
+        <Card key={id} price={24 / 24} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+type Props = {
+  price: number;
+};
+
+export const Card: FC<Props> = () => {
+  return <div>index</div>;
+};
+
+export default Card;
